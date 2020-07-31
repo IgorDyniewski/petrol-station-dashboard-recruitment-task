9a2d38db9eedbb59092b096cf57c3c94da4a3faf
Igor Dyniewski <igor.dyniewski@gmail.com>
2020-07-31 15:40:07 +0200

    Mapbox engine works and renders markers

---
+++ src/lib/mapBoxConstants.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mapboxApiAccessToken:
+        'pk.eyJ1IjoibWFydGluMTIzNHh4IiwiYSI6ImNrZDh2aWl1NjB5MDkyeHNjZmE0aG8zeTgifQ.Ems9-6kwnaDj88FMg4nhfw',
+    styleUrl: 'mapbox://styles/martin1234xx/ckd8w9zn200cq1ikc7je8ln38',
+}
