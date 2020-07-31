63adedb5d130b91fbd3e4aad0a0118b1f0fd2367
Igor Dyniewski <igor.dyniewski@gmail.com>
2020-07-31 18:12:24 +0200

    Added available petrol types

--- src/lib/data/petrolStationData.js
+++ src/lib/data/petrolStationData.js
@@ -12,6 +12,7 @@ const availablePetrolStations = [
         name: 'Rosenberg Petrol Station',
         logoUrl:
             'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Open_Source_Initiative_keyhole.svg/1024px-Open_Source_Initiative_keyhole.svg.png',
+        availablePetrolTypes: ['B7', 'PB 98', 'PB 95', 'LPG'],
     },
     {
         id: 1,
@@ -19,8 +20,8 @@ const availablePetrolStations = [
         lon: -95.016849,
         subTitle: 'TX 6',
         name: 'Hitchcock Petrol Station',
-        logoUrl:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Open_Source_Initiative_keyhole.svg/1024px-Open_Source_Initiative_keyhole.svg.png',
+        logoUrl: 'https://image.flaticon.com/icons/svg/25/25719.svg',
+        availablePetrolTypes: ['B7', 'PB 98', 'PB 95', 'LPG'],
     },
     {
         id: 2,
@@ -30,6 +31,7 @@ const availablePetrolStations = [
         name: 'Old River Road Petrol Station',
         logoUrl:
             'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Open_Source_Initiative_keyhole.svg/1024px-Open_Source_Initiative_keyhole.svg.png',
+        availablePetrolTypes: ['B7', 'PB 98', 'PB 95', 'LPG'],
     },
     {
         id: 3,
@@ -37,8 +39,8 @@ const availablePetrolStations = [
         lon: -94.208519,
         subTitle: 'Smith Road',
         name: 'Cheek Petrol Station',
-        logoUrl:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Open_Source_Initiative_keyhole.svg/1024px-Open_Source_Initiative_keyhole.svg.png',
+        logoUrl: 'https://image.flaticon.com/icons/svg/25/25719.svg',
+        availablePetrolTypes: ['B7', 'PB 98', 'PB 95', 'LPG'],
     },
 ]
 
